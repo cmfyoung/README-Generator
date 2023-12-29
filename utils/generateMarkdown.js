d1301e1286fcb416db19ad26b55cf1bd925ba372
cmfyoung <cmfyoung7@gmail.com>
2023-12-29 10:57:30 -0500

    Fixed Badges and TOC

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,9 +4,18 @@ const fs = require('fs');
 function renderLicenseBadge (license) {
   if (!license) {
     return '';
+  } else if (license ==="MIT"){
+  return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
   }
-
-  return `[[Static Badge](https://img.shields.io/badge/:badgeContent)](https://opensource.org/licenses/${license})`;
+  else if (license ==="GNU3"){
+  return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)IT-yellow.svg)"
+  }
+ else if (license ==="Apache"){
+  return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+  }
+  else if (license ==="MPL2"){
+  return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+  }
 }
 
 // TODO: Create a function that returns the license link
@@ -23,6 +32,8 @@ function renderLicenseLink(license) {
 function renderLicenseSection(license) {
   if (!license) {
     return '';
+  } else if (license === "MIT") {
+    return "## License \n https://opensource.org/license/mit/"
   }
 
   return `[License](https://opensource.org/licenses/${license})`;
